228fabf44de99defac5d3591474e4b12cf5e37e2
uspehgp <uspehgp@gmail.com>
2021-10-14 18:53:30 +0300

    create debugging for form value

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {EquationComponent} from './equation/equation.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  })
+
+  get a() {
+    return this.mathForm.value.a
+  }
+
+  get b() {
+    return this.mathForm.value.b
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 100)
+  }
+
+}
